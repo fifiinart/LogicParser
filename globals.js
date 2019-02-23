e54088b56ef8cdbb32a0c136ab2e61df04b3cd1f
Fifi Art <44811907+fifiinart@users.noreply.github.com>
2019-02-23 17:38:59 -0500

    Create globals.js

---
+++ globals.js
@@ -0,0 +1,30 @@
+valueIn = function(item, array) {
+  for (let i of array) {
+    slice = array.slice(i, i + item.length);
+    if (i === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function range(start, end, by = 1) {
+  let y = [];
+  for (let x = start; x < end; x += by) {
+    y.push(x);
+  }
+  return y;
+}
+
+function center(value, length, fill = " ") {
+  let padding = length - value.length;
+  let start = Math.ceil(padding / 2);
+  let end = padding - start;
+  return value.padStart(value.length + start, fill).padEnd(value.length + start + end, fill);
+}
+
+module.exports = {
+  valueIn,
+  range,
+  center
+};
